d3cee88a965aeab9ae2b182bf767ddb16263b2a9
Jan Metelański <metelanskij@gmail.com>
2023-10-19 01:06:19 +0200

    deploy

--- controllers/register.js
+++ controllers/register.js
@@ -28,7 +28,7 @@ const handleRegister = (req, res, knex, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback);
     })
-    .catch((err) => res.status(400).json('Unable to register'));
+    .catch((err) => res.status(400).json(err));
 };
 module.exports = {
   handleRegister: handleRegister,
